25e0ec450c06ab3fabfc568521c3116632c4fcc2
Stefan (DB4SCW) <dev@db4scw.de>
2025-08-05 13:51:00 +0200

    add comments

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
+//define electron
 const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+
+//get path
 const path = require('path');
+
+//get flrig handler
 const flrig = require('./flrig/flrigControl');
 
+//create window
 function createWindow() {
   const win = new BrowserWindow({
     width: 600,
-    height: 550,
+    height: 800,
     icon: path.join(__dirname, 'assets', 'parrrot_icon_512.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
@@ -14,10 +20,16 @@ function createWindow() {
     }
   });
 
+  //load UI
   win.loadFile('renderer/index.html');
-  win.webContents.openDevTools();
+  
+  //load dev tools when needed
+  //win.webContents.openDevTools();
 }
  
+//define handlers
+
+//define file handler
 ipcMain.handle('select-file', async () => {
   const result = await dialog.showOpenDialog({
     filters: [{ name: 'Audio Files', extensions: ['mp3', 'wav'] }],
@@ -26,11 +38,15 @@ ipcMain.handle('select-file', async () => {
   return result.filePaths[0] || null;
 });
 
+//define flrig behaviour
 ipcMain.handle('flrig:enableTX', () => flrig.enableTX());
 ipcMain.handle('flrig:disableTX', () => flrig.disableTX());
 ipcMain.handle('flrig:getMode', () => flrig.getMode());
 
+//create window
 app.whenReady().then(createWindow);
+
+//handler for app close
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
--- preload.js
+++ preload.js
@@ -1,9 +1,12 @@
+//BridgetoMailWorld
 const { contextBridge, ipcRenderer } = require('electron');
 
+//explose file handling
 contextBridge.exposeInMainWorld('electronAPI', {
   selectFile: () => ipcRenderer.invoke('select-file')
 });
 
+//expose flrig handling
 contextBridge.exposeInMainWorld('flrigAPI', {
   enableTX: () => ipcRenderer.invoke('flrig:enableTX'),
   disableTX: () => ipcRenderer.invoke('flrig:disableTX'),
